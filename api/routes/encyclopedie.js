6783d2cf6771cee1be8cd2200891a0600b980241
Thomas <thomas.letellier28500@gmail.com>
2023-03-24 17:31:42 +0100

    <feat> Début de la tâche de filtrage

--- api/routes/encyclopedie.js
+++ api/routes/encyclopedie.js
@@ -2,7 +2,7 @@ const db = require('../bdd.js');
 
 module.exports = {
     getEncyclopedie: function(req, res) {
-        db.query('SELECT i.idImage, i.libelleImage, r.idRace, r.libelleRace FROM image i JOIN race r ON i.idRaceImage = r.idRace GROUP BY r.idRace', function(err, result, fields) {
+        db.query('SELECT i.idImage, i.libelleImage, r.idRace, r.libelleRace, A.libelle FROM image i JOIN race r ON i.idRaceImage = r.idRace INNER JOIN alimentation A ON idAlimentation = id GROUP BY r.idRace', function(err, result, fields) {
             if (err) throw err;
             res.json(result);
         });
--- src/pages/Encyclopedie.js
+++ src/pages/Encyclopedie.js
@@ -11,16 +11,46 @@ const Encyclopedie = () => {
             setData(response.data);
         })
     })
+
+    const liItem = document.querySelectorAll('ul li');
+    const imgItem = document.querySelectorAll('.card img');
+
+    liItem.forEach(li => {
+        li.onclick = function () {
+            //active
+            liItem.forEach(li => {
+                li.className = "";   
+            })
+            li.className = "active";
+
+            //filtre
+            const value = li.textContent;
+            imgItem.forEach(img => {
+            img.style.display = "none";
+            if(img.getAttribute("data-filter") == value.toLowerCase()){
+                img.style.display = "block";
+            }
+            })
+        }
+    });
     return (
         <div className="container page">
             <h1><strong>Encyclopédie</strong></h1>
             <hr></hr>
             <h2><strong>Liste de nos animaux</strong></h2>
-            <div className="container ">
+            <div className="container">
+                <div className="row filter_card">
+                    <ul>
+                        <li class="active">Tous</li>
+                        <li>Carnivore</li>
+                        <li>Omnivore</li>
+                        <li>Herbivore</li>
+                    </ul>
+                </div>
                 <div className="row img_animal">                       
                             {data.map((item) => (
                                 <div className="col-sm card" id={item.idRace} key={item.idRace} name={item.libelleRace}>
-                                    <a href=""><img className="image" src={"http://localhost:3300/api/src/images/"+item.libelleImage}></img>
+                                    <a href={"/animal?"+item.libelleRace}><img className="image" src={"http://localhost:3300/api/src/images/"+item.libelleImage} data-filter={item.libelle}></img>
                                     <div className='overlay'>
                                         <div className="text">{item.libelleRace}</div>
                                     </div></a>
