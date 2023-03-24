3209112cf3bd402a84e0862780e286c86db219fa
Oliwer Skweres <swerk.pro@gmail.com>
2023-03-24 12:05:26 +0100

    <Feat>(Login): Page de connexion
    Page de connexion pour les utilisateurs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm';
+import './Login.css'
+import logo from '../logo.svg';
 
 const Login = (props) => {
+
+
+    if(localStorage.getItem('user')) {
+        window.location.href = "/home";
+    }
+
     return (
-        <div>
+        <div className="container">
+            <div className="card card-margin">
+                <div className="card-header">
+                    <div className="d-flex justify-content-center">
+                        <img className="img-40vw" src={logo} alt="logo.svg"></img>
+                    </div>
+                </div>
+                <LoginForm></LoginForm>
+            </div>
+            
         </div>
     );
 };
