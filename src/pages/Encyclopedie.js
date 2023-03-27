33f8434f7774e08adb2745ef8a4b5acec3f09e1d
Thomas <thomas.letellier28500@gmail.com>
2023-03-27 15:35:23 +0200

    <feat> Fin du filtre par ALIMENTATION

--- src/pages/Encyclopedie.js
+++ src/pages/Encyclopedie.js
@@ -1,36 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import css from '../pages/encyclopedie.css';
+import './encyclopedie.css';
 
 const Encyclopedie = () => {
     const [data, setData] = useState([]);
+    const [data2, setData2] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:3300/api/encyclopedie") /* lien pour récupérer valeur requête */
         .then((response) => {
             setData(response.data);
+            setData2(response.data);
         })
-    })
+    },[])
 
     const liItem = document.querySelectorAll('ul li');
-    const imgItem = document.querySelectorAll('.card img');
+    const imgItem = document.getElementsByTagName('img');
 
     liItem.forEach(li => {
         li.onclick = function () {
+            
+            //reintialisation de data
+            setData(data2);
+
             //active
             liItem.forEach(li => {
-                li.className = "";   
+                li.className = "";
             })
             li.className = "active";
 
             //filtre
-            const value = li.textContent;
-            imgItem.forEach(img => {
-            img.style.display = "none";
-            if(img.getAttribute("data-filter") == value.toLowerCase()){
-                img.style.display = "block";
+            let filter = li.textContent;
+            console.log(filter);
+            let array = [];
+            data2.forEach((item) => {
+            if(item.libelle == filter){
+                array.push(item);
+            }else if (filter == "Tous"){
+                array.push(item);
             }
             })
+            setData(array);
         }
     });
     return (
@@ -50,7 +60,7 @@ const Encyclopedie = () => {
                 <div className="row img_animal">                       
                             {data.map((item) => (
                                 <div className="col-sm card" id={item.idRace} key={item.idRace} name={item.libelleRace}>
-                                    <a href={"/animal?"+item.libelleRace}><img className="image" src={"http://localhost:3300/api/src/images/"+item.libelleImage} data-filter={item.libelle}></img>
+                                    <a href={"/animal?"+item.libelleRace}><img className="image" id={item.libelleRace} src={"http://localhost:3300/api/src/images/"+item.libelleImage} name={item.libelle}></img>
                                     <div className='overlay'>
                                         <div className="text">{item.libelleRace}</div>
                                     </div></a>
