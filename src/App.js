46e08dd9a9d6faf8dd62813ec99b9251fe2ad2a8
Oliwer Skweres <swerk.pro@gmail.com>
2023-03-31 11:36:52 +0200

    <Feat>(Loader): Ajout d'un loader

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import Nav from './components/Nav';
 import Admin from './pages/Admin'
 import Accounts from './pages/Accounts'
 import MissionAccueil from './pages/MissionAccueil';
+import Loader from './components/Loader';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
       
       <BrowserRouter>
+      {loading && <Loader />}
         <Nav />
         <Routes>
           <Route path="/" element={<Login />} />
---
+++ src/components/Loader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import './Loader.scss'
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const LoaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${fadeOut} 1s forwards;
+  animation-delay: ${({ delay }) => delay}ms;
+  z-index: 9999;
+`;
+
+const Loader = () => {
+  return (
+    <LoaderWrapper delay={400}>
+      <div class="loader"></div>
+    </LoaderWrapper>
+  );
+};
+
+export default Loader;
